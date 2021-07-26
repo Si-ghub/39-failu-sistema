776859a25b707c719807fe9d094ddb68bce76cd3
Si-ghub <63141977+Si-ghub@users.noreply.github.com>
2021-07-26 19:56:56 +0300

    Update

--- index.js
+++ index.js
@@ -6,55 +6,27 @@ _data.read('users', 'petras', (err, data) => {
         return false;
     }
 
-    const obj = JSON.parse(data); // JSON.parse()paverciam objektu // JSON.stringify() paverciam tekstu
-    // console.log(obj);
-    // console.log(obj.name);
-    // console.log(obj.age);
-    // console.log(obj.car);
+    const obj = JSON.parse(data);
+    console.log(obj);
 })
 
-// funkcija callback priima err, data
-_data.read('users', 'petras', (err, data) => {
-    if (err || !data) {
-        console.log('Nepavyko perskaityti failo...');
-        return false;
-    }
 
-    console.log(`Stai mano duomenys is failo: petras`);
-    console.log(data);
-})
-
-_data.read('users', 'maryte', (err, data) => {
-    if (err || !data) {
-        console.log('Nepavyko perskaityti failo...');
-        return false;
-    }
-    console.log(`Stai mano duomenys is failo: maryte`);
-    console.log(data);
-})
-
-_data.read('users', 'jonas', (err, data) => {
-    if (err || !data) {
-        console.log('Nepavyko perskaityti failo...');
-        return false;
-    }
-    console.log(`Stai mano duomenys is failo: maryte`);
-    console.log(data);
-})
-_data.read('users', 'niekas', (err, data) => {
-    if (err || !data) {
-        console.log('Nepavyko perskaityti failo...');
+const pazymiai = [10, 2, 8, 4, 6];
+_data.create('marks', 'kazys', pazymiai, (err) => {
+    if (err) {
         console.log(err);
         return false;
     }
-    console.log(data);
-})
 
-_data.read('users2', 'niekas', (err, data) => {
-    if (err || !data) {
-        console.log('Nepavyko perskaityti failo...');
-        console.log(err);
-        return false;
-    }
-    console.log(data);
-})
+    console.log('Failas sekmingai sukurtas!');
+
+    _data.read('marks', 'kazys', (err, data) => {
+        if (err || !data) {
+            console.log('Nepavyko perskaityti Kazio failo...');
+            return false;
+        }
+
+        const obj = JSON.parse(data);
+        console.log(obj);
+    })
+});
