9417c7a0fd274e272a0f6c69d9db4e49ac8568eb
Si-ghub <63141977+Si-ghub@users.noreply.github.com>
2021-07-30 18:51:46 +0300

    List

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const _data = require('./lib/data');
 const helpers = require('./lib/helpers')
 
-_data.read('users', 'petras', (err, data) => {
-    if (err || !data) {
-        console.log('Nepavyko perskaityti failo...');
-        return false;
-    }
+// _data.read('users', 'petras', (err, data) => {
+//     if (err || !data) {
+//         console.log('Nepavyko perskaityti failo...');
+//         return false;
+//     }
 
-    console.log(data);
-    const obj = helpers.parseJsonToObject(data);
-    console.log(obj);
-    //try catch naudojamas kaip apsauga, pabando ir parodo jei yra klaida
-    // let obj = 0;
-    // try {
-    //     obj = JSON.parse(data); //pabandom perskaityti kas parasyta
-    // } catch (err) {
-    //     obj = {};
-    // }
+//     console.log(data);
+//     const obj = helpers.parseJsonToObject(data);
+//     console.log(obj);
+//     //try catch naudojamas kaip apsauga, pabando ir parodo jei yra klaida
+//     // let obj = 0;
+//     // try {
+//     //     obj = JSON.parse(data); //pabandom perskaityti kas parasyta
+//     // } catch (err) {
+//     //     obj = {};
+//     // }
 
-    // console.log(obj);
-})
+//     // console.log(obj);
+// })
 
 
 // const pazymiai = [10, 2, 8, 4, 6];
@@ -75,5 +75,14 @@ _data.read('users', 'petras', (err, data) => {
 //     }
 //     console.log('Failas sekmingai istrintas');
 // })
+
+_data.list('marks', (err, data) => {
+    if (err || !data) {
+        console.log(err);
+        return false;
+    }
+
+    console.log(data);
+})
 
 
